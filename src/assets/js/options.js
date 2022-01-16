bac65fb6b4de1b3270687f1b1546fcf3f29a6461
Varun Sridharan <varunsridharan23@gmail.com>
2022-01-16 08:23:11 +0530

    Improved Popup

--- src/assets/js/options.js
+++ src/assets/js/options.js
@@ -1,17 +1,19 @@
 $( document ).ready( () => {
 	const formElm = $( 'form' );
-	browser.storage.sync.get( [ 'homer_dashboard_url', 'homer_dashboard_popup_height', 'homer_dashboard_popup_width' ] )
+	browser.storage.sync.get( [ 'homer_dashboard_url', 'homer_dashboard_popup_height', 'homer_dashboard_popup_width', 'homer_dashboard_popup_scale' ] )
 		   .then( ( result ) => {
 			   let url = result.homer_dashboard_url;
 			   $( 'input#dashboardurl' ).val( url );
 			   $( 'input#popup_height' ).val( result.homer_dashboard_popup_height );
 			   $( 'input#popup_width' ).val( result.homer_dashboard_popup_width );
+			   $( 'input#popup_scale' ).val( result.homer_dashboard_popup_scale );
 		   } );
 	formElm.on( 'submit', () => {
 		browser.storage.sync.set( {
 			'homer_dashboard_url': $( 'input#dashboardurl' ).val(),
 			'homer_dashboard_popup_height': $( 'input#popup_height' ).val(),
 			'homer_dashboard_popup_width': $( 'input#popup_width' ).val(),
+			'homer_dashboard_popup_scale': $( 'input#popup_scale' ).val(),
 		} );
 		return false;
 	} );
--- src/assets/js/popup.js
+++ src/assets/js/popup.js
@@ -1,8 +1,8 @@
-browser.storage.sync.get( [ 'homer_dashboard_url', 'homer_dashboard_popup_height', 'homer_dashboard_popup_width' ] )
+browser.storage.sync.get( [ 'homer_dashboard_url', 'homer_dashboard_popup_height', 'homer_dashboard_popup_width','homer_dashboard_popup_scale' ] )
 	   .then( ( result ) => {
-		   let scale   = 0.75;
-		   let width   = result.homer_dashboard_popup_width || 320;
-		   let height  = result.homer_dashboard_popup_height || 480;
+		   let scale   = parseFloat(result.homer_dashboard_popup_scale) || 0.80;
+		   let width   = parseInt(result.homer_dashboard_popup_width) || 420;
+		   let height  = parseInt(result.homer_dashboard_popup_height) || 900;
 		   let wrapper = document.getElementById( 'spaceWrap' );
 		   let iframe  = document.getElementById( 'homer-dashboard-frame' );
 
@@ -10,8 +10,8 @@ browser.storage.sync.get( [ 'homer_dashboard_url', 'homer_dashboard_popup_height
 			   wrapper.style.setProperty( 'width', width + 'px' );
 			   wrapper.style.setProperty( 'height', height + 'px' );
 		   } else {
-			   wrapper.style.setProperty( 'width', Math.round( ( width * scale ) ) + 'px' );
-			   wrapper.style.setProperty( 'height', Math.round( ( height * scale ) ) + 'px' );
+			   wrapper.style.setProperty( 'width', ( width * scale ) + 'px' );
+			   wrapper.style.setProperty( 'height', ( height * scale ) + 'px' );
 		   }
 
 		   iframe.style.setProperty( 'width', width + 'px' );
@@ -20,7 +20,5 @@ browser.storage.sync.get( [ 'homer_dashboard_url', 'homer_dashboard_popup_height
 		   iframe.style.setProperty( '-ms-transform', 'scale(' + scale + ')' );
 		   iframe.style.setProperty( 'transform', 'scale(' + scale + ')' );
 
-		   setTimeout( function() {
-			   document.getElementById( 'homer-dashboard-frame' ).setAttribute( 'src', result.homer_dashboard_url );
-		   }, 100 );
+		   document.getElementById( 'homer-dashboard-frame' ).setAttribute( 'src', result.homer_dashboard_url );
 	   } );
